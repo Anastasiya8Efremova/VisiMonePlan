1c028cb3bb6f40ef3843feb109248c545ee7c7a3
Kamilayza <efremova0anastasia0serg0@gmail.com>
2024-10-21 06:21:40 +0300

    Добавьте файлы проекта.

---
+++ VisiMonePlanSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisiMonePlanSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
